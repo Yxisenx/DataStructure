418da0c8fa9887d5cb291c7f4387acadd5950211
yangbiao <yangbiaocc@outlook.com>
2022-11-25 13:50:28 +0800

    feat: Binary search tree

--- src/main/java/cn/onecolour/dataStructure/tree/binary_tree/BinarySearchTree.java
+++ src/main/java/cn/onecolour/dataStructure/tree/binary_tree/BinarySearchTree.java
@@ -1,6 +1,9 @@
 package cn.onecolour.dataStructure.tree.binary_tree;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.stream.Collectors;
 
 /**
@@ -8,6 +11,7 @@ import java.util.stream.Collectors;
  * @date 2022/11/15
  * @description BST
  */
+@SuppressWarnings("UnusedReturnValue")
 public class BinarySearchTree<T extends Comparable<T>> {
     private Node<T> root;
     private int size;
@@ -17,7 +21,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
     }
 
     public BinarySearchTree(T root) {
-        this.root = new Node<T>(root, null, null);
+        this.root = new Node<>(root, null, null);
     }
 
     public void insert(T ele) {
@@ -28,7 +32,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 
     private Node<T> insertRecursive(Node<T> root, T ele) {
         if (root == null) {
-            root = new Node<T>(ele, null, null);
+            root = new Node<>(ele, null, null);
         } else if (root.getVal().compareTo(ele) > 0) {
             root.setLeft(insertRecursive(root.getLeft(), ele));
         } else {
@@ -39,7 +43,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 
     private Node<T> insertLoop(Node<T> root, T ele) {
         if (root == null) {
-            root = new Node<T>(ele, null, null);
+            root = new Node<>(ele, null, null);
             return root;
         }
 
@@ -55,20 +59,21 @@ public class BinarySearchTree<T extends Comparable<T>> {
             }
         }
         Node<T> newNode = new Node<>(ele, null, null);
-        if (pre.getVal().compareTo(ele) > 0) {
+        int compareVal = pre.getVal().compareTo(ele);
+        if (compareVal > 0) {
             pre.setLeft(newNode);
-        } else {
+        } else if (compareVal < 0) {
             pre.setRight(newNode);
         }
         return root;
     }
 
 
-
     public void preorderTraverseByRecursive() {
         preorderTraverseByRecursive(root);
         System.out.println();
     }
+
     private void preorderTraverseByRecursive(Node<T> node) {
         System.out.print(node.getVal() + ",");
         if (node.hasLeftChild()) {
@@ -113,6 +118,10 @@ public class BinarySearchTree<T extends Comparable<T>> {
     }
 
     public void inorderTraverseByLoop() {
+        System.out.println(inorderList().stream().map(Object::toString).collect(Collectors.joining(",", "", ",")));
+    }
+
+    public List<T> inorderList() {
         LinkedList<Node<T>> stack = new LinkedList<>();
         List<T> result = new ArrayList<>(size);
 
@@ -126,7 +135,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
             result.add(temp.getVal());
             temp = temp.getRight();
         }
-        System.out.println(result.stream().map(Object::toString).collect(Collectors.joining(",", "", ",")));
+        return result;
     }
 
 
@@ -143,6 +152,321 @@ public class BinarySearchTree<T extends Comparable<T>> {
             postorderTraverseByRecursive(node.getRight());
         }
         System.out.print(node.getVal() + ",");
+    }
+
+    public void postorderTraverseByLoop() {
+        LinkedList<Node<T>> stack = new LinkedList<>();
+        List<T> result = new ArrayList<>(size);
+
+        Node<T> temp = root;
+        Node<T> pre = null;
+        while (temp != null || !stack.isEmpty()) {
+            while (temp != null) {
+                stack.push(temp);
+                temp = temp.getLeft();
+            }
+            temp = stack.pop();
+            if (temp.getRight() == null || pre == temp.getRight()) { // 访问节点的条件
+                result.add(temp.getVal()); // 访问
+                pre = temp; // 这一步是记录上一次访问的节点
+                temp = null; // 此处为了跳过下一次循环的访问左子节点的过程，直接进入栈的弹出阶段，因为但凡在栈中的节点，它们的左子节点都肯定被经过且已放入栈中。
+            } else { // 不访问节点的条件
+                stack.push(temp); // 将已弹出的根节点放回栈中
+                temp = temp.getRight(); // 经过右子节点
+            }
+
+        }
+        System.out.println(result.stream().map(Object::toString).collect(Collectors.joining(",", "", ",")));
+    }
+
+
+    private boolean depthFirstSearch(T ele) {
+        Node<T> temp = root;
+
+        while (temp != null) {
+            int compareTo = temp.getVal().compareTo(ele);
+            if (compareTo > 0) {
+                temp = temp.getLeft();
+            } else if (compareTo < 0) {
+                temp = temp.getRight();
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean breadthFirstSearch(T ele) {
+        Queue<Node<T>> queue = new LinkedList<>();
+        if (root != null) {
+            queue.offer(root);
+        }
+        Node<T> temp;
+        while ((temp = queue.poll()) != null) {
+            if (ele.equals(temp.getVal())) {
+                return true;
+            } else {
+                if (temp.hasLeftChild()) {
+                    queue.offer(temp.getLeft());
+                }
+                if (temp.hasRightChild()) {
+                    queue.offer(temp.getRight());
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * search in bst
+     * @return if ele exists in bst return 'true', otherwise 'false'
+     */
+    public boolean search(T ele) {
+        return search(ele, SearchType.DFS);
+    }
+
+    public boolean search(T ele, SearchType type) {
+        switch (type) {
+            case DFS:
+                return depthFirstSearch(ele);
+            case BFS:
+                return breadthFirstSearch(ele);
+            default:
+                throw new IllegalArgumentException("Unknown search type.");
+        }
+    }
+
+
+    public enum SearchType {
+        BFS, // Breadth-first search
+        DFS // Depth first search
+    }
+
+
+    /**
+     * delete an element from the bst.
+     * <p>the node to be deleted is a leaf.</p>
+     * <p>&nbsp&nbsp&nbsp&nbsp instead the node of null</p>
+     * <p>the node has just one child.</p>
+     * <p>&nbsp&nbsp&nbsp&nbsp replace the node as the child of the node </p>
+     * <p>the node has two child.</p>
+     * <p>&nbsp&nbsp&nbsp&nbsp First, find the inorder successor of the node to be deleted.</p>
+     * <p>&nbsp&nbsp&nbsp&nbsp After that, replace that node with the inorder successor until the target node is placed at the leaf of tree.</p>
+     * <p>&nbsp&nbsp&nbsp&nbsp And at last, replace the node with NULL and free up the allocated space.</p>
+     * <p></p>
+     * <p>delete by moving node</p>
+     */
+    private boolean deleteV1(T ele) {
+        Node<T> parent = null;
+        Node<T> cur = root;
+        while (cur != null) {
+            int compareVal = ele.compareTo(cur.getVal());
+            // equals
+            if (compareVal == 0) {
+                if (parent == null) {
+                    // remove root (cur -> root)
+                    if (cur.hasRightChild() && cur.hasLeftChild()) {
+                        // two child
+                        // successor is the minimum node of the right subtree
+                        Node<T> successorP = null;
+                        Node<T> successor = cur.getRight();
+                        while (successor.hasLeftChild()) {
+                            successorP = successor;
+                            successor = successor.getLeft();
+                        }
+                        if (successorP == null) {
+                            // successor is cur right child
+                            successor.setLeft(root.getLeft());
+                            clearNode(root);
+                            root = successor;
+                        } else {
+                            if (successor.hasRightChild()) {
+                                // is not a leaf
+                                successorP.setLeft(successor.getRight());
+                                successor.setLeft(root.getLeft());
+                                successor.setRight(root.getRight());
+                                clearNode(root);
+                                root = successor.getRight();
+                            } else {
+                                // is a leaf
+                                successorP.setLeft(null);
+                                successor.setLeft(root.getLeft());
+                                successor.setRight(root.getRight());
+                                clearNode(root);
+                                root = successor;
+                            }
+                        }
+                    } else if (cur.hasLeftChild() || cur.hasRightChild()) {
+                        // one child
+                        Node<T> child = cur.hasLeftChild() ? cur.getLeft() : cur.getRight();
+                        clearNode(root);
+                        root = child;
+                    } else {
+                        // no child
+                        clearNode(root);
+                        root = null;
+                    }
+                } else {
+                    if (cur.hasRightChild() && cur.hasLeftChild()) {
+                        // two child
+                        boolean curIsLeftChild = cur.equals(parent.getLeft());
+                        // successor is the minimum node of the right subtree
+                        Node<T> successorP = null;
+                        Node<T> successor = cur.getRight();
+                        // find minimum
+                        while (successor.hasLeftChild()) {
+                            successorP = successor;
+                            successor = successor.getLeft();
+                        }
+
+                        if (successorP == null) {
+                            // successor is right of cur.
+                            if (curIsLeftChild) {
+                                parent.setLeft(successor);
+                            } else {
+                                parent.setRight(successor);
+                            }
+                            successor.setLeft(cur.getLeft());
+                        } else {
+                            successorP.setLeft(successor.hasRightChild() ? successor.getRight() : null);
+                            if (curIsLeftChild) {
+                                parent.setLeft(successor);
+                            } else {
+                                parent.setRight(successor);
+                            }
+                            successor.setLeft(cur.getLeft());
+                            successor.setRight(cur.getRight());
+                        }
+                        clearNode(cur);
+
+
+                    } else if (cur.hasLeftChild() || cur.hasRightChild()) {
+                        // one child
+                        if (cur.equals(parent.getLeft())) {
+                            parent.setLeft(cur.hasLeftChild() ? cur.getLeft() : cur.getRight());
+                        } else {
+                            parent.setRight(cur.hasLeftChild() ? cur.getLeft() : cur.getRight());
+                        }
+                        clearNode(cur);
+                    } else {
+                        // no child
+                        if (cur.equals(parent.getLeft())) {
+                            parent.setLeft(null);
+                        } else {
+                            parent.setRight(null);
+                        }
+                        cur.setVal(null);
+                    }
+                }
+                return true;
+            } else {
+                parent = cur;
+                cur = compareVal > 0 ? cur.getRight() : cur.getLeft();
+            }
+        }
+        return false;
+    }
+
+    public boolean delete(T ele) {
+        return deleteV2(ele);
+    }
+
+    public boolean delete(T ele, DeleteType type) {
+        boolean result;
+        switch (type) {
+            case V1:
+                result = deleteV1(ele);
+                break;
+            case V2:
+                result = deleteV2(ele);
+                break;
+            default:
+                throw new IllegalArgumentException("Not found the type.");
+        }
+        if (result) {
+            size--;
+        }
+        return result;
+    }
+
+    private boolean deleteV2(T ele) {
+        Node<T> parent = null;
+        Node<T> cur = root;
+        while (cur != null) {
+            int compareVal = ele.compareTo(cur.getVal());
+            // equals
+            if (compareVal == 0) {
+                if (cur.hasLeftChild() || cur.hasRightChild()) {
+                    if (cur.hasLeftChild() && cur.hasRightChild()) {
+                        // has two child
+                        // find successor( the minimum node of the subtree whose root node is the right child of cur), inorder
+                        Node<T> successor = cur.getRight();
+                        Node<T> successorParent = cur;
+                        // minimum
+                        while (successor.hasLeftChild()) {
+                            successorParent = successor;
+                            successor = successor.getLeft();
+                        }
+                        // change value of cur with successor
+                        T temp = cur.getVal();
+                        cur.setVal(successor.getVal());
+                        successor.setVal(temp);
+
+                        boolean successorIsLeftChild = successor.equals(successorParent.getLeft());
+                        if (successorIsLeftChild) {
+                            successorParent.setLeft(successor.getRight());
+                        } else {
+                            successorParent.setRight(successor.getRight());
+                        }
+                        clearNode(successor);
+                    } else {
+                        // has one child, replace cur with the child
+                        Node<T> child = cur.hasLeftChild() ? cur.getLeft() : cur.getRight();
+                        if (parent != null) {
+                            if (cur.equals(parent.getLeft())) {
+                                parent.setLeft(child);
+                            } else {
+                                parent.setRight(child);
+                            }
+                            clearNode(cur);
+                        } else {
+                            // cur is the root node
+                            clearNode(root);
+                            root = child;
+                        }
+                    }
+                } else {
+                    // is a leaf
+                    if (parent != null) {
+                        // cur is different with root
+                        if (cur.equals(parent.getLeft())) {
+                            // cur is left child of parent
+                            parent.setLeft(null);
+                        } else {
+                            parent.setRight(null);
+                        }
+                    } else {
+                        root = null;
+                    }
+                    clearNode(cur);
+                }
+                return true;
+            } else {
+                parent = cur;
+                cur = compareVal > 0 ? cur.getRight() : cur.getLeft();
+            }
+        }
+        return false;
+    }
+
+    public enum DeleteType {
+        V1, V2
+    }
+
+    private void clearNode(Node<T> node) {
+        node.setLeft(null);
+        node.setRight(null);
+        node.setVal(null);
     }
 
 }
--- src/test/java/cn/onecolour/dataStructure/tree/binary_tree/BSTTest.java
+++ src/test/java/cn/onecolour/dataStructure/tree/binary_tree/BSTTest.java
@@ -1,11 +1,15 @@
 package cn.onecolour.dataStructure.tree.binary_tree;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author yang
@@ -14,30 +18,109 @@ import java.util.List;
  */
 public class BSTTest {
 
-    private final static List<Integer> integers = Arrays.asList(4,2,6,1,3,5,7);
+    private final static List<Integer> integers = Arrays.asList(4, 2, 6, 1, 3, 5, 7);
 
     private static BinarySearchTree<Integer> bst;
+    private static BinarySearchTree<Integer> bst2;
 
     @BeforeAll
     static void before() {
+        init();
+    }
+
+    private static void init() {
         bst = new BinarySearchTree<>();
+        bst2 = new BinarySearchTree<>();
+
         for (Integer integer : integers) {
             bst.insert(integer);
+            bst2.insert(integer);
         }
     }
 
     @Test
+    void searchTest() {
+        System.out.println(bst.search(-1));
+        System.out.println(bst.search(1));
+        System.out.println(bst.search(7, BinarySearchTree.SearchType.BFS));
+    }
+
+    /**
+     * 前序遍历
+     */
+    @Test
     void preorderRecursiveTest() {
+        // 前序递归
         bst.preorderTraverseByRecursive();
+        // 前序非递归
         bst.preorderTraverseByLoop();
     }
+
+    /**
+     * 中序遍历
+     */
     @Test
     void inorderRecursiveTest() {
+        // 中序递归
         bst.inorderTraverseByRecursive();
+        // 中序非递归
         bst.inorderTraverseByLoop();
     }
+
+    /**
+     * 后续遍历
+     */
     @Test
     void postorderRecursiveTest() {
+        // 后续递归
         bst.postorderTraverseByRecursive();
+        // 后续非递归
+        bst.postorderTraverseByLoop();
+    }
+
+    @Test
+    void deleteFromBstTest() {
+        Random random = new Random();
+        for (int j = 0; j < 10000; j++) {
+            int deleteNum = random.nextInt(7) + 1;
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < deleteNum;) {
+                Integer integer = integers.get(random.nextInt(7));
+                if (!list.contains(integer)) {
+                    list.add(integer);
+                    i++;
+                }
+            }
+//            System.out.println(list);
+            init();
+            for (Integer integer : list) {
+                bst.delete(integer, BinarySearchTree.DeleteType.V1);
+                bst2.delete(integer, BinarySearchTree.DeleteType.V2);
+            }
+            List<Integer> list1 = bst.inorderList();
+            List<Integer> list2 = bst2.inorderList();
+            assertAll("remove_result",
+//                    () -> System.out.printf("%s\n%s\n==================\n", list1, list2),
+                    () -> assertTrue(list1.containsAll(list2)),
+                    () -> assertTrue(list2.containsAll(list1)));
+        }
+
+    }
+
+    @Test
+    void deleteV2Test() {
+        for (Integer integer : integers) {
+            bst2.delete(integer, BinarySearchTree.DeleteType.V2);
+        }
+        System.out.println(bst2.inorderList());
+    }
+
+    @Test
+    void deleteV1Test() {
+        List<Integer> integers = Arrays.asList(1, 3, 2, 4, 5, 7);
+        for (Integer integer : integers) {
+            bst.delete(integer, BinarySearchTree.DeleteType.V1);
+        }
+        System.out.println(bst.inorderList());
     }
 }
